e99bb8d7198a2cfd1c2c5935e27ac6265816a420
Sean <hello@sgservices.ae>
2025-07-03 22:43:44 +0400

    PartsStep.tsx text change

--- src/components/dashboard/OrderModal/PartsStep.tsx
+++ src/components/dashboard/OrderModal/PartsStep.tsx
@@ -124,7 +124,7 @@ export const PartsStep: React.FC<PartsStepProps> = ({
             </div>
             
             <div>
-              <Label htmlFor="estimatedBudget">Max Budget (Optional)</Label>
+              <Label htmlFor="estimatedBudget">Max Budget (excluding VAT & service fee)</Label>
               <div className="relative mt-1">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                   <DollarSign className="h-4 w-4 text-gray-400" />
